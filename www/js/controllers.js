253ee33870c7fc9bc792e7a9c7d2fef43ea84f3b
Scott James <AustinJames@Scotts-MacBook-Air-2.local>
2018-10-14 22:08:20 -0400

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,44 @@
+angular.module('app.controllers', [])
+
+  .controller('calcCtrl', function($scope) {
+
+    // Initialize the screen to ""
+    $scope.screen="";
+
+    // Add the selected number to the screen
+    $scope.zero = function() {$scope.screen+="0";};
+    $scope.one = function() {$scope.screen+="1";};
+    $scope.two = function() {$scope.screen+="2";};
+    $scope.three = function() {$scope.screen+="3";};
+    $scope.four = function() {$scope.screen+="4";};
+    $scope.five = function() {$scope.screen+="5";};
+    $scope.six = function() {$scope.screen+="6";};
+    $scope.seven = function() {$scope.screen+="7";};
+    $scope.eigth = function() {$scope.screen+="8";};
+    $scope.nine = function() {$scope.screen+="9";};
+
+    // Add the selected operator to the screen
+    $scope.plus = function() {$scope.screen+="+";};
+    $scope.minus = function() {$scope.screen+="-";};
+    $scope.multiply = function() {$scope.screen+="*";};
+    $scope.divide = function() {$scope.screen+="/";};
+    $scope.clear = function() {$scope.screen="";};
+
+   // Evaluate the expression in the screen
+   $scope.equal=function(){$scope.screen=eval($scope.screen);};
+
+  })
+
+  .controller('indexCtrl', function($scope,$state) {
+    $state.go('calculator', {}, {location: "replace"});
+  })
+
+/*
+  .controller('settingsCtrl', function($scope) {
+
+  })
+*/
+
+  .controller('supportCtrl', function($scope) {
+
+  })
